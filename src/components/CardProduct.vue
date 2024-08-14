cf7972dfe59ea8e48e8de075a11ed38a33fcbe60
Ravenagatha18 <ravenagatha18@gmail.com>
2024-08-14 15:03:12 +0700

    membuat halaman home

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      :src="product.photo"
+      class="card-img-top"
+      :alt="product.name || 'Product image'"
+    />
+    <div class="card-body text-center">
+      <h5 class="card-title">{{ product.name }}</h5>
+      <p class="card-text">Rp.{{ product.price }}</p>
+      <a href="#" class="btn btn-success">ADD TO CART</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,27 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import Navbar from "../components/Navbar.vue";
 import Dashboard from "../components/Dashboard.vue";
+import CardProduct from "../components/CardProduct.vue";
+import axios from "axios";
+
+const products = ref([]);
+
+function setProduct(data) {
+  products.value = data;
+}
+
+onMounted(() => {
+  axios
+    .get("https://sistemtoko.com/public/demo/product")
+    .then(function (response) {
+      console.log("berhasil", response.data.aaData);
+      setProduct(response.data.aaData);
+    })
+    .catch(function (error) {
+      console.error("Error fetching products:", error);
+    });
+});
 </script>
 
 <template>
@@ -8,7 +29,14 @@ import Dashboard from "../components/Dashboard.vue";
     <Navbar />
     <div class="container-fluid">
       <Dashboard />
-      <div class="row"></div>
+      <div class="container">
+        <div class="row mt-3">
+          <div class="col-3 mt-4" v-for="product in products" :key="product.id">
+            <CardProduct :product="product" />
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
+
